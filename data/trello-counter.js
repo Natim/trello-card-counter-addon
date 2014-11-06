c4e0ad00f29fd3c3116ea9c8016d6fbb8249dc8e
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-11-06 17:01:08 +0100

    Adding a refresh when entering the tab.

--- data/trello-counter.js
+++ data/trello-counter.js
@@ -2,7 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var list = document.getElementsByClassName("list-header-num-cards");
-for (var i = 0; i < list.length; i++) {
-  list[i].classList.remove("hide");
-}
+self.port.on("cardCounterRefresh", function() {
+  var list = document.getElementsByClassName("list-header-num-cards");
+  for (var i = 0; i < list.length; i++) {
+    list[i].classList.remove("hide");
+  }
+});
